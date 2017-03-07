321fd6efe63f2dde052eee76de649fa1b0b22daa
Giovanni Beri <giovanniberi93@gmail.com>
2017-03-07 17:14:32 +0100

    [FILE ADDITION] Button driver added
     Addition of the source and header for the button driver
    
    Signed-off-by: Giovanni Beri <giovanniberi93@gmail.com>

---
+++ miosix-kernel/button.cpp
@@ -0,0 +1,55 @@
+
+#include "button.h"
+#include <miosix.h>
+#include <miosix/kernel/scheduler/scheduler.h>
+
+using namespace miosix;
+
+typedef Gpio<GPIOA_BASE,0> button;
+
+static Thread *waiting=0;
+
+void __attribute__((naked)) EXTI0_IRQHandler()
+{
+    saveContext();
+    asm volatile("bl _Z16EXTI0HandlerImplv");
+    restoreContext();
+}
+
+
+void __attribute__((used)) EXTI0HandlerImpl()
+{
+    EXTI->PR=EXTI_PR_PR0;
+    if(waiting==0) return;
+    // task become schedulable again
+    waiting->IRQwakeup();
+    if(waiting->IRQgetPriority()>Thread::IRQgetCurrentThread()->IRQgetPriority())
+		Scheduler::IRQfindNextThread();
+    waiting=0;
+}
+
+void init()
+{
+    button::mode(Mode::INPUT_PULL_DOWN);
+    EXTI->IMR |= EXTI_IMR_MR0;
+    EXTI->RTSR |= EXTI_RTSR_TR0; 
+    NVIC_EnableIRQ(EXTI0_IRQn);
+    NVIC_SetPriority(EXTI0_IRQn,15); //Low priority
+}
+
+// task
+void waitForButton()
+{
+    // disable interrupt
+    FastInterruptDisableLock dLock;
+    waiting=Thread::IRQgetCurrentThread();
+    while(waiting)
+    {
+        // call a preemption, return only when someone wake me up (sw interrupt
+        Thread::IRQwait();
+        // enable interrupt only in the scope
+        FastInterruptEnableLock eLock(dLock);
+        // dont return until someone wake me up
+        Thread::yield();
+    }
+}
