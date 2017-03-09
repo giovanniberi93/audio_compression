c113c9bd52c823d69ab858b15653eca61d9527e9
Giovanni Beri <giovanniberi93@gmail.com>
2017-03-09 15:53:52 +0100

    [DESKTOP_SIDE] Fixed bug that caused missing of some samples
    Signed-off-by: Giovanni Beri <giovanniberi93@gmail.com>

--- miosix-kernel/desktop_app.py
+++ miosix-kernel/desktop_app.py
@@ -6,15 +6,13 @@
 #
 # Usage: plot.py device baudrate filename
 #
-# Esample: ./plot.py /dev/ttyUSB0 19200
+# Esample: ./plot.py /dev/ttyUSB0 19200 myFile
 #
 
 import sys, serial, re
 
 beginSignal = "new"
-usage = """Usage: plot.py device baudrate
-
-plot.py /dev/ttyUSB0 19200 myFile""";
+usage = "Usage: " + str(sys.argv[0]) +" device baudrate myFile" + "\n\nexample:\n"+ sys.argv[0] + " /dev/ttyUSB0 19200 myFile";
 
 if len(sys.argv) < 4:
 	print("Too few arguments")
@@ -25,22 +23,29 @@ if ser < 1:
 	print("Error opening serial port")
 	print(usage)
 	exit( 1 )
-outputFile = open(sys.argv[3],"a");
+outputFile = open(sys.argv[3],"w");
 
 # pattern = re.compile("(-)?\d+(.\d+)?:(-)?\d+(.\d+)?");
-pattern = re.compile("(-)?\d+(.\d+)?");
+pattern = re.compile("(-)?\d+");
 sample = ""
 while sample != beginSignal: #wait for the begin signal
 	sample = ser.readline().split("\r\n")[0]
-print(sample)
+first = True
 while True:
 	sample = ser.readline()
-	while sample != beginSignal:
-		sample = ser.readline().split("\r\n")[0]
-		# print("sample is $", sample)
+	# print("1: sample is $"+ sample)
+	while sample != beginSignal :
+		if first == True:
+			first = False
+			sample = sample.split("\r\n")[0]
+		else :
+			sample = ser.readline().split("\r\n")[0]
 		match = pattern.match(sample) 
 		if match != None:
+			# here all and only samples different from beginSignal are processed
 			sample = match.group()
+			print("sample is $"+ sample)
 			outputFile.write(sample+" ")
 		else :
-			print "# Regular expression error: ",sample 
+			first = True
+			# print "# Regular expression error: ",sample 
