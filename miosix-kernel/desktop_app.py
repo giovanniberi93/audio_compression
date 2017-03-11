d82b47643ddeb77c46c379aba863d77c5171be48
Giovanni Beri <giovanniberi93@gmail.com>
2017-03-11 12:25:49 +0100

    [DESKTOP SIDE] Writing in binary file
    Now the python script writes a binary file with each sample in a fixed number of bits.
    NOTE: updated the script to python3 in order to have some byte-level operation at disposal
    
    Signed-off-by: Giovanni Beri <giovanniberi93@gmail.com>

--- miosix-kernel/desktop_app.py
+++ miosix-kernel/desktop_app.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 #
 # This script plots vectors received via serial as couples of 
 # indexes and values saparated by a semicolon. Each vector is
@@ -19,33 +19,37 @@ if len(sys.argv) < 4:
 	print(usage)
 	exit( 1 )
 ser = serial.Serial(str(sys.argv[1]), int(sys.argv[2])) #no timeout specified
-if ser < 1:
-	print("Error opening serial port")
-	print(usage)
-	exit( 1 )
-outputFile = open(sys.argv[3],"w");
+# if 	ser.isOpen()
+# 	print("Error opening serial port")
+# 	print(usage)
+# 	exit( 1 )
+outputFile = open(sys.argv[3],"wb");
 
-# pattern = re.compile("(-)?\d+(.\d+)?:(-)?\d+(.\d+)?");
+#size of the sample 
+bytes_per_int = 2
+
 pattern = re.compile("(-)?\d+");
 sample = ""
 while sample != beginSignal: #wait for the begin signal
-	sample = ser.readline().split("\r\n")[0]
+	sample = ser.readline().decode();
+	sample = sample.split("\r\n")[0]
 first = True
 while True:
-	sample = ser.readline()
+	sample = ser.readline().decode()
 	# print("1: sample is $"+ sample)
 	while sample != beginSignal :
 		if first == True:
 			first = False
 			sample = sample.split("\r\n")[0]
 		else :
-			sample = ser.readline().split("\r\n")[0]
+			sample = ser.readline().decode().split("\r\n")[0]
 		match = pattern.match(sample) 
 		if match != None:
-			# here all and only samples different from beginSignal are processed
+			# here all and only the samples are processed
 			sample = match.group()
+			intSample = int(sample)
 			print("sample is $"+ sample)
-			outputFile.write(sample+" ")
+			outputFile.write(intSample.to_bytes(bytes_per_int, 'little', signed=True))
 		else :
 			first = True
 			# print "# Regular expression error: ",sample 
