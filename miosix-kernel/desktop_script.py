a9c2434b33be18a22e1cb3ffb995251fc875dfea
Giovanni Beri <giovanniberi93@gmail.com>
2017-03-18 16:26:14 +0100

    [DESKTOP SIDE] Raw data handling, and improved efficiency in writing on the file
    Signed-off-by: Giovanni Beri <giovanniberi93@gmail.com>

--- miosix-kernel/desktop_script.py
+++ miosix-kernel/desktop_script.py
@@ -9,14 +9,9 @@
 #
 
 import sys, serial, re
-import ctypes
-
-def int16_to_uint16(i):
-    return ctypes.c_uint16(i).value
-
 
 beginSignal = "new"
-usage = "Usage: " + str(sys.argv[0]) +" device baudrate myFile.raw" + "\n\nexample:\n"+ sys.argv[0] + " /dev/ttyUSB0 19200 myFile.raw";
+usage = "Usage: " + str(sys.argv[0]) +" device baudrate myFile.raw" + "\n\nexample:\n"+ sys.argv[0] + " /dev/ttyUSB0 921600 myFile.raw";
 
 if len(sys.argv) < 4:
 	print("Too few arguments")
@@ -25,32 +20,21 @@ if len(sys.argv) < 4:
 ser = serial.Serial(str(sys.argv[1]), int(sys.argv[2])) #no timeout specified
 
 outputFile = open(sys.argv[3],"wb");
+outputFileTxt = open(sys.argv[3] + ".txt", "w")
 
-#size of the sample 
-bytes_per_int = 2
-
-pattern = re.compile("(-)?\d+");
 sample = ""
 while sample != beginSignal: #wait for the begin signal
 	sample = ser.readline().decode()
 	sample = sample.split("\r\n")[0]
-first = True
-while True:
-	sample = ser.readline().decode()
-	while sample != beginSignal :
-		if first == True :
-			first = False
-			sample = sample.split("\r\n")[0]
-		else :
-			sample = ser.readline().decode().split("\r\n")[0]
-		match = pattern.match(sample) 
-		if match != None:
-			# here all and only the samples are processed
-			sample = match.group()
-			intSample = int(sample)
-			adjustedIntSample = int16_to_uint16(intSample)
+# read the batch size
+size = ser.readline().decode();
+# because every sample is made of 2 bytes
+bytesPerBatch = int(size)*2
+print("bytes per batch: ",bytesPerBatch)
 
-			print("sample is $", intSample, "adjusted to ",adjustedIntSample)
-			outputFile.write(adjustedIntSample.to_bytes(bytes_per_int, 'little', signed=False))
-		else :
-			first = True
+while True:
+	sample = ser.read(size=bytesPerBatch)
+	print("sample is ",sample)
+	# samples are in little endian
+	outputFile.write(sample)
+	outputFileTxt.write(str(int.from_bytes(sample,byteorder='little', signed=False))+"\n")
