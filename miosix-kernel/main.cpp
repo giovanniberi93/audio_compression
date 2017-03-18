32e1b5f7f728328ecc44300232e3e64a95b5ff71
Giovanni Beri <giovanniberi93@gmail.com>
2017-03-18 13:14:17 +0100

    [COMMUNICATION] Improved communication efficiency, by using stdout in raw mode
    Signed-off-by: Giovanni Beri <giovanniberi93@gmail.com>

--- miosix-kernel/main.cpp
+++ miosix-kernel/main.cpp
@@ -5,33 +5,45 @@
 #include "button.h"
 #include <math.h>
 #include <functional>
+#include <termios.h>
 
 using namespace std;
 using namespace miosix;
 
+typedef Gpio<GPIOD_BASE,13> orangeLed;
 
 void visualize(unsigned short* PCM, unsigned short  size){
-    iprintf("new\n");
-    for(int i = 0; i < size; i++){
-        // outputs unsigned value of the samples
-        iprintf("%d\n",(short) PCM[i] );
-    }
+    // if(PCM[1] > 50000)
+    //     orangeLed::high();
+    // else
+    //     orangeLed::low();
+    fwrite(PCM, sizeof(unsigned short int), size, stdout);
+}
+
+void setRawStdout(){
+    // configure stdout in raw mode
+    struct termios t;
+    tcgetattr(STDOUT_FILENO,&t);
+    t.c_lflag &= ~(ISIG | ICANON);    
 }
 
 int main()
 {
-    Microphone& mic = Microphone::instance(); 
 
+    orangeLed::mode(Mode::OUTPUT);
+    setRawStdout();
     /* Best results obtained with a size in the form of (N + 0.5) * 256 with N integer */
-    static const unsigned short size = 384;
-    // mic.init(&visualize,size);
+    static const unsigned short size = 10;
+
+    Microphone& mic = Microphone::instance(); 
     mic.init(bind(&visualize,placeholders::_1,placeholders::_2), size);
     buttonInit();
 
+    // starting procedure
     waitForButton();
     mic.start();
     ledOn();
-
+    // ending procedure
     waitForButton();
     mic.stop();
     ledOff();
