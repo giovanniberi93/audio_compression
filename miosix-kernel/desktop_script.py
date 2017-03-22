705add4a96dc018250158d5f309a9fb01e6c7d52
Giovanni Beri <giovanniberi93@gmail.com>
2017-03-22 10:54:11 +0100

    [DESKTOP SIDE] Fixed raw mode handling, according to the changes on firmware side (\n not translated to \r\n anymore)
    Signed-off-by: Giovanni Beri <giovanniberi93@gmail.com>

--- miosix-kernel/desktop_script.py
+++ miosix-kernel/desktop_script.py
@@ -5,27 +5,31 @@
 #
 # Usage: desktop_script.py device baudrate filename
 #
-# Example: ./desktop_script.py /dev/ttyUSB0 19200 myFile
+# Example: ./desktop_script.py /dev/ttyUSB0 115200 myFile
 #
 
 import sys, serial, re
 
 beginSignal = "new"
-usage = "Usage: " + str(sys.argv[0]) +" device baudrate myFile.raw" + "\n\nexample:\n"+ sys.argv[0] + " /dev/ttyUSB0 921600 myFile.raw";
+usage = "Usage: " + str(sys.argv[0]) +" device baudrate myFile.raw" + "\n\nexample:\n"+ sys.argv[0] + " /dev/ttyUSB0 115200 myFile.raw";
 
 if len(sys.argv) < 4:
 	print("Too few arguments")
 	print(usage)
 	exit( 1 )
-ser = serial.Serial(str(sys.argv[1]), int(sys.argv[2])) #no timeout specified
+
+# setup serial port
+ser = serial.Serial(str(sys.argv[1]), int(sys.argv[2]),stopbits=serial.STOPBITS_ONE,parity=serial.PARITY_NONE, bytesize=serial.EIGHTBITS) #no timeout specified
 
 outputFile = open(sys.argv[3],"wb");
 outputFileTxt = open(sys.argv[3] + ".txt", "w")
 
+print("prima del begin signal")
 sample = ""
 while sample != beginSignal: #wait for the begin signal
 	sample = ser.readline().decode()
-	sample = sample.split("\r\n")[0]
+	sample = sample.split("\n")[0]
+print("dopo il begin signal")
 # read the batch size
 size = ser.readline().decode();
 # because every sample is made of 2 bytes
@@ -34,7 +38,8 @@ print("bytes per batch: ",bytesPerBatch)
 
 while True:
 	sample = ser.read(size=bytesPerBatch)
-	print("sample is ",sample)
+	intSample = int.from_bytes(sample,byteorder='little', signed=False)
+	print("sample is ",str(intSample))
 	# samples are in little endian
 	outputFile.write(sample)
-	outputFileTxt.write(str(int.from_bytes(sample,byteorder='little', signed=False))+"\n")
+	outputFileTxt.write(str(intSample)+"\n")
