6d67b4ba11c242fd61edfa1f347c43a6fbacd3b9
Janzo <giovanniberi93@gmail.com>
2017-04-11 16:40:34 +0200

    [MP3 ENCODER] Added files of Shine mp3 encoder
    Signed-off-by: Janzo <giovanniberi93@gmail.com>

---
+++ miosix-kernel/mp3Encoder/l3mdct.h
@@ -0,0 +1,8 @@
+#ifndef shine_MDCT_H
+#define shine_MDCT_H
+
+// void shine_mdct_initialise();
+void shine_mdct_initialise(shine_global_config *config);
+void shine_mdct_sub(shine_global_config *config, int stride);
+
+#endif
