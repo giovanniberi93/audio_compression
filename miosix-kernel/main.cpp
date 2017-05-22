e929155b281573e1e283b049b74b132474e4e9b1
Janzo <giovanniberi93@gmail.com>
2017-05-23 01:24:00 +0200

    [MAIN] Added board-side implementation of data transmission
    Signed-off-by: Janzo <giovanniberi93@gmail.com>

--- miosix-kernel/main.cpp
+++ miosix-kernel/main.cpp
@@ -1,8 +1,3 @@
-// write e raw
-// /////sdpcm//// adpcm
-// bitvuket /aturri/btphone
-
-
 #include <cstdio>
 #include "miosix.h"
 #include "Microphone.h"
@@ -10,21 +5,19 @@
 #include <math.h>
 #include <functional>
 #include <termios.h>
+#include <string.h>
 
 using namespace std;
 using namespace miosix;
 
-typedef Gpio<GPIOD_BASE,13> orangeLed;
 
-
-unsigned short b[] = {10,10,10,10,10,10,10,10};
-// unsigned short b[] = {10};
-
-void sendToSerial(unsigned short* PCM, unsigned short size){
-        write(STDOUT_FILENO,b,sizeof(unsigned short)*size);
-        // fflush(stdout); 
-
+void sendToSerial(unsigned char* compressedData, unsigned int size){
+    // send actual size of the batch
+    write(STDOUT_FILENO,&size,sizeof(int));
+    // send the batch of data
+    write(STDOUT_FILENO,compressedData,size);
 }
+
 
 // configure stdout in raw mode
 void setRawStdout(){
@@ -35,30 +28,27 @@ void setRawStdout(){
 }
 
 
-// RICORDATI CHE HAI ATTIVATO IL FLOW CONTROL DA BOARD_SETTINGS.H
-// simple handshake with desktop script
-void serialHandshake(int size){
-    iprintf("new\n");
-    iprintf("%d\n",size);
+void sendInitSignal(int expectedBatchSize){
+    // send signal to desktop script
+    char init[] = "ready\n";
+    write(STDOUT_FILENO,init,strlen(init));
+    // send expected size of the data batches
+    write(STDOUT_FILENO,&expectedBatchSize,sizeof(int));
 }
+
 
 int main()
 {
-    orangeLed::mode(Mode::OUTPUT);
-    setRawStdout();
-    // configSerial();
-    // Best results obtained with a size in the form of (N + 0.5) * 256 with N integer
-    static const unsigned short size = 8;
-
     Microphone& mic = Microphone::instance(); 
-    mic.init(bind(sendToSerial,placeholders::_1,placeholders::_2), size);
+    mic.init(bind(sendToSerial,placeholders::_1,placeholders::_2));
     buttonInit();
+    setRawStdout();
 
-    // starting procedure
     waitForButton();
-    serialHandshake(size);
+    sendInitSignal(mic.getBatchSize());
     mic.start();
     ledOn();
+
     // ending procedure
     waitForButton();
     mic.stop();
